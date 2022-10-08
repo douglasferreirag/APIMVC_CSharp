209c2bba2d402d69ded0fe2511aa560671801b13
Douglas <douglas.ferreirag@gmail.com>
2022-10-07 22:57:13 -0300

    Configurando o Entity Framework

---
+++ Models/Contato.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto.Models
+{
+    public class Contato
+    {
+        
+            public int Id { get; set; }
+
+            public string Nome { get; set; }
+
+            public string Telefone { get; set; }
+
+            public bool Ativo { get; set; }
+
+
+
+    }
+}
