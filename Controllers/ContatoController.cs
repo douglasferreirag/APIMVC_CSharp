c0257d3eab363757fd7d49607caa9c3aa6f532b4
Douglas <douglas.ferreirag@gmail.com>
2022-10-10 21:11:34 -0300

    Criando a pagina de deletar

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -95,6 +95,18 @@ namespace Projeto.Controllers
 
              }
 
+            public IActionResult Deletar(int id){
+
+                       var contato = _context.Contatos.Find (id);
+
+                       if (contato == null)
+
+                            return RedirectToAction(nameof (Index));
+
+                        return View(contato);
+
+             }
+
 
 
     }
