b8a8237f7edf1a0e2c0886089acb0fe597b184e3
Douglas <douglas.ferreirag@gmail.com>
2022-10-10 23:03:02 -0300

    Alterando o menu

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Projeto.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
